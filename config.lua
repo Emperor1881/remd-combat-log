9f789c6d821c844390cff4ce443e6eedd4f65edf
Emperor1881 <72162538+Emperor1881@users.noreply.github.com>
2023-09-01 15:51:48 +0300

    Update config.lua

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ Config = {}
 Config.DrawingTime = 60*1000 --60 seconds
 Config.TextColor = {r=255, g=255,b=255} -- WHITE (Player Data)
 Config.AlertTextColor = {r=255, g=0, b=0} -- RED (Player Left Game)
-Config.LogSystem = true -- Add webhook on server.lua
+Config.LogSystem = false -- Add webhook on server.lua
 Config.UseSteam = true -- If False then use R* License
 Config.LogBotName = "remd-combat-log"
 Config.AutoDisableDrawing = false
